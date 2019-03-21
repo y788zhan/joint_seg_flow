f498a0be05bf2305bd8c7339cfbb24c25493f28b
Richard Zhang <y788zhan@uwaterloo.ca>
2019-03-21 17:21:07 -0400

    multiscale

--- src/util/TrainingLoss.py
+++ src/util/TrainingLoss.py
@@ -10,34 +10,56 @@ class TrainingLoss:
 			lossComponents = instanceParams["lossComponents"]
 
 			# helpers
-			predFlowF = networkF.flows[0]
-			predFlowB = networkB.flows[0]
 			frame0 = trainingData.frame0
 			frame1 = trainingData.frame1
 			vpm = trainingData.validMask
 
-			# unsup loss
-			pLossF, sLossF, segLossF = unsupFlowLoss(predFlowF,predFlowB,frame0,frame1,vpm,instanceParams)
-			recLossF = pLossF + sLossF + segLossF
-			if lossComponents["backward"]:
-				pLossB, sLossB, segLossB = unsupFlowLoss(predFlowB,predFlowF,frame1,frame0,vpm,instanceParams, backward=True)
-				recLossB = pLossB + sLossB + segLossB
-			# weight decay
-			with tf.variable_scope(None,default_name="weightDecay"):
-				weightLoss = tf.reduce_sum(tf.stack([tf.nn.l2_loss(i) for i in tf.get_collection("weights")]))*weightDecay
+            pLossTotalF, sLossTotalF, segLossTotalF = 0
+            pLossTotalB, sLossTotalB, segLossTotalB = 0
 
+            rel_weights = [1, 0.32, 0.08, 0.02, 0.01, 0.005]
+			# unsup loss
+            for i in range(len(networkF.flows)):
+                predFlowF = networkF.flows[i]
+                predFlowB = networkB.flows[i]
+
+                if i > 0:
+                    predFlowF *= 20
+                    predFlowB *= 20
+
+    			pLossF, sLossF, segLossF = unsupFlowLoss(predFlowF,predFlowB,frame0,frame1,vpm,instanceParams)
+                pLossF *= rel_weights[i]
+                sLossF *= rel_weights[i]
+                segLossF *= rel_weights[i]
+
+                pLossTotalF += pLossF
+                sLossTotalF += sLossF
+                segLossTotalF += segLossF
+
+    			if lossComponents["backward"]:
+    				pLossB, sLossB, segLossB = unsupFlowLoss(predFlowB,predFlowF,frame1,frame0,vpm,instanceParams, backward=True)
+                    pLossB *= rel_weights[i]
+                    sLossB *= rel_weights[i]
+                    segLossB *= rel_weights[i]
+
+                    pLossTotalB += pLossB
+                    sLossTotalB += sLossB
+                    segLossTotalB += segLossB
+
+
+            regLossF = pLossTotalF + sLossTotalF + segLossTotalF
+            regLossB = pLossTotalB + sLossTotalB + segLossTotalB
 			# final loss, schedule backward unsup loss
 			recLossBWeight = 0.5  #  [0,0.5]
 			self.recLossBWeight = recLossBWeight # used to set weight at runtime
 			if lossComponents["backward"]:
 				totalLoss = \
 					recLossF*(1.0 - recLossBWeight) + \
-					recLossB*recLossBWeight + \
-					weightLoss
+					recLossB*recLossBWeight
 				tf.summary.scalar("recLossF",tf.reduce_mean(recLossF*(1.0-recLossBWeight)))
 				tf.summary.scalar("recLossB",tf.reduce_mean(recLossB*recLossBWeight))
 			else:
-				totalLoss = recLossF + weightLoss
+				totalLoss = recLossF
 				tf.summary.scalar("recLossF",tf.reduce_mean(recLossF))
 
 			pGrad = tf.gradients(pLossF, predFlowF)[0]

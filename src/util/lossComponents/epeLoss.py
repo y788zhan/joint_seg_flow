8a912b88a3034fab858ed5f4a86fb2f28eaebca4
Richard Zhang <y788zhang@RSG-PC301.cs.uwaterloo.ca>
2019-04-02 14:51:15 -0400

    epe loss function

---
+++ src/util/lossComponents/epeLoss.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from components import *
+import numpy as np
+
+ground_truth = np.expand_dims(np.load("/home/y788zhang/train_hs_lambda10000.npy"), 0)[:,16:-16,11:-11,:]
+ground_truth = tf.cast(ground_truth, tf.float32)
+
+def epeLoss(flow):
+	with tf.variable_scope(None, default_name="epeLoss"):
+		size = [flow.shape[1].value, flow.shape[2].value]
+		scale = 448 / size[0]
+		flowDiff = flow - tf.image.resize_bilinear(ground_truth / (2 ** (scale - 1)), size)
+		flowDist = charbonnierLoss(flowDiff, 1, 1, 0.001)
+		return tf.reduce_sum(flowDist, axis=3, keep_dims=True)
